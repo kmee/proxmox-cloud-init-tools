468ad2a022041ec8782e6e76c7599a121d56d2f2
Ananias Filho <ananias@kmee.com.br>
2021-02-16 09:32:08 -0300

    [ADD] Add CentOS 8 Cloud Image to install

--- deploy.sh
+++ deploy.sh
@@ -31,6 +31,7 @@ DEBIAN_9_URL="https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-op
 UBUNTU_1804_URL="https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img"
 UBUNTU_2004_URL="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
 OPENSUSE_152_URL="https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.2/images/openSUSE-Leap-15.2-OpenStack.x86_64.qcow2"
+CENTOS_8_URL="https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
 ####
 echo "Available images are: "
 echo -n "
@@ -39,6 +40,7 @@ echo -n "
 3 - Ubuntu 18.04 LTS - Bionic
 4 - Ubuntu 20.04 LTS - Focal
 5 - OpenSUSE LEAP 15.02
+6 - CentOS 8
 "
 echo -n "Choose a Image template to install: "
 read OPT_IMAGE_TEMPLATE
@@ -72,6 +74,12 @@ case $OPT_IMAGE_TEMPLATE in
 		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${OPENSUSE_152_URL##*/}"
 		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
 			wget -c $OPENSUSE_152_URL -O $TEMPLATE_VM_CI_IMAGE
+		fi
+		;;
+	6)
+		TEMPLATE_VM_CI_IMAGE="$IMG_PATH/${CENTOS_8_URL##*/}"
+		if [ ! -f $TEMPLATE_VM_CI_IMAGE ]; then
+			wget -c $CENTOS_8_URL -O $TEMPLATE_VM_CI_IMAGE
 		fi
 		;;
 	*)
